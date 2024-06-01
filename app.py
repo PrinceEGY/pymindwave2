0c39f97756319f8546aec5485fa0dd52b1798920
Ahmed Mohsen <ahmed25004@hotmail.com>
2024-06-01 04:48:15 +0300

    add: save logs to file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from util.logger import Logger
 
 from mindwave.headset import MindWaveMobile2
 
-Logger(level=logging.DEBUG)
+Logger(level=logging.DEBUG, filename="mindwave.log", filemode="w")
 headset = MindWaveMobile2()
 headset.connect(n_tries=3, timeout=20)
 while True:
--- util/logger.py
+++ util/logger.py
@@ -4,20 +4,24 @@ import logging
 class Logger:
     _instance = None
 
-    def __new__(cls, level=logging.DEBUG, *args, **kwargs):
+    def __new__(cls, level=logging.DEBUG, filename=None, filemode="w"):
         if cls._instance is None:
-            cls._instance = super(Logger, cls).__new__(cls, *args, **kwargs)
-            cls._instance._configure_logger(level)
+            cls._instance = super(Logger, cls).__new__(cls)
+            cls._instance._configure_logger(level, filename, filemode)
         return cls._instance
 
-    def _configure_logger(self, level):
+    def _configure_logger(self, level, filename, filemode):
         self.logger = logging.getLogger()
-        handler = logging.StreamHandler()
+        st_handler = logging.StreamHandler()
+        f_handler = logging.FileHandler(filename, mode=filemode)
+        f_handler.setLevel(level)
         formatter = logging.Formatter(
             "%(levelname)s:%(asctime)s %(name)s: %(message)s", datefmt="%H:%M:%S"
         )
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+        f_handler.setFormatter(formatter)
+        st_handler.setFormatter(formatter)
+        self.logger.addHandler(st_handler)
+        self.logger.addHandler(f_handler)
         self.logger.setLevel(level)
 
     def get_logger(self, name):
