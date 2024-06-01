900021ffbf41ee8afbb3b9419eb9121da1816c04
Ahmed Mohsen <ahmed25004@hotmail.com>
2024-06-01 06:37:31 +0300

    test: stream parser using print

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import logging
+from mindwave.data import Data
+from util.event_handler import EventType
 from util.logger import Logger
 
 from mindwave.headset import MindWaveMobile2
@@ -6,5 +8,8 @@ from mindwave.headset import MindWaveMobile2
 Logger(level=logging.DEBUG, filename="mindwave.log", filemode="w")
 headset = MindWaveMobile2()
 headset.connect(n_tries=3, timeout=20)
+
+
+headset.add_listener(event_type=EventType.HeadsetData, listener=print)
 while True:
     pass
