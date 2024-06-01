b65a92d1067796e595a6c3ec08f15403c6889c33
Ahmed Mohsen <ahmed25004@hotmail.com>
2024-06-01 06:32:04 +0300

    renaming attributes to match streamed data

--- mindwave/data.py
+++ mindwave/data.py
@@ -4,18 +4,18 @@ from util.logger import Logger
 class Data:
     def __init__(self):
         self._logger = Logger._instance.get_logger(self.__class__.__name__)
-        self.raw_data = []
+        self.raw_data = [0] * 512
         self.attention = 0
         self.meditation = 0
 
         self.delta = 0
         self.theta = 0
-        self.low_alpha = 0
-        self.high_alpha = 0
-        self.low_beta = 0
-        self.high_beta = 0
-        self.low_gamma = 0
-        self.high_gamma = 0
+        self.lowAlpha = 0
+        self.highAlpha = 0
+        self.lowBeta = 0
+        self.highBeta = 0
+        self.lowGamma = 0
+        self.highGamma = 0
 
     def update_data(self, **kwargs):
         for key, value in kwargs.items():
