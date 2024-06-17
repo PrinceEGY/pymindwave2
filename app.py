0493cc370e6c1dca092135993d8006455e9c9e64
Ahmed Mohsen <ahmed25004@hotmail.com>
2024-06-17 21:02:27 +0300

    add: start acq minimal logic

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import sys
 import threading
 from PySide6 import QtWidgets
 from mindwave.headset import MindWaveMobile2
+from mindwave.session import SessionConfig
 from ui.main_window import Ui_MainWindow
+from ui.error_dialog import ErrorDialog
 from PySide6.QtWidgets import QMainWindow
 
 from util.connection_state import ConnectionStatus
@@ -26,6 +28,7 @@ class MainWindow(QMainWindow):
 
         self.ui.pushButton_savedir.clicked.connect(self.get_folder_path)
         self.ui.pushButton_headset_connect.clicked.connect(self.headset_connection)
+        self.ui.pushButton_start_session.clicked.connect(self.start_acquisition)
 
     def get_folder_path(self):
         folder_path = QtWidgets.QFileDialog.getExistingDirectory()
@@ -33,10 +36,44 @@ class MainWindow(QMainWindow):
 
     def headset_connection(self):
         if self.headset.connection_status == ConnectionStatus.DISCONNECTED:
-            thread = threading.Thread(target=self.headset.connect, daemon=True)
+            thread = threading.Thread(
+                target=self.headset.connect, daemon=True, kwargs={"timeout": 30}
+            )
         else:
             thread = threading.Thread(target=self.headset.disconnect, daemon=True)
         thread.start()
+
+    def start_acquisition(self):
+        if self.headset.connection_status != ConnectionStatus.CONNECTED:
+            self._logger.error("Headset is not connected")
+            error_dialog = ErrorDialog("Headset is not connected!")
+            error_dialog.exec()
+        elif self.headset.signal_quality < 100:
+            self._logger.error(
+                "Signal quality is low, Current Signal Quality: %s",
+                self.headset.signal_quality,
+            )
+            error_dialog = ErrorDialog(
+                "Signal quality is low!, Please wear the headset properly and make sure the signal quality is 100% before starting the session"
+            )
+            error_dialog.exec()
+
+        sess_config = SessionConfig(
+            user_name=self.ui.lineEdit_username.text(),
+            user_age=self.ui.spinBox_Age.value(),
+            user_gender=self.ui.comboBox_gender.currentText(),
+            classes=self.ui.lineEdit_classes.text().split(","),
+            trials=self.ui.spinBox_trials.value(),
+            baseline_duration=float(self.ui.lineEdit_baseline.text()),
+            rest_duration=float(self.ui.lineEdit_rest.text()),
+            ready_duration=float(self.ui.lineEdit_ready.text()),
+            cue_duration=float(self.ui.lineEdit_cue.text()),
+            motor_duration=float(self.ui.lineEdit_motor.text()),
+            extra_duration=float(self.ui.lineEdit_extra.text()),
+            save_dir=self.ui.lineEdit_savedir.text(),
+            capture_blinks=self.ui.checkBox_blinks.isChecked(),
+        )
+        self._logger.debug("Session Configuration: %s", sess_config)
 
     def update_headset_status(self, status):
         # TODO: change the button status logic to be consisted with the retrying connection logic
